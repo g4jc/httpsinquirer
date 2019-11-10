683934d36550b79dcbab22b037c22e0c5f17d448
Gaming4JC <g4jc@hyperbola.info>
2019-11-10 17:44:31 -0500

    Add a basic build script

---
+++ makexpi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VERSION="0.92"
+sed -i "/<em:version>/c\        <em:version>$VERSION</em:version>" install.rdf
+
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir pkg
+fi
+cd pkg
+7z a -tzip -mx9 -mm=Deflate -mfb=258 -mmt=8 -mpass=15 -mtc=on "httpsinquirer-$VERSION.xpi" ../* -x@../.ignore
+cd ..
+echo "Generated "httpsinquirer-$VERSION.xpi" placed in pkg/"
+sha512sum pkg/*
+echo "Done!"
